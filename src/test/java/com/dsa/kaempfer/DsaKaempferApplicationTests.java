f57f09545bd8023533955f357d8f137d16387d11
TheTrueHonker <daniel.wemmer.dw@gmail.com>
2024-09-15 13:36:43 +0200

    chore: add spring boot project

---
+++ src/test/java/com/dsa/kaempfer/DsaKaempferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.kaempfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaKaempferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
